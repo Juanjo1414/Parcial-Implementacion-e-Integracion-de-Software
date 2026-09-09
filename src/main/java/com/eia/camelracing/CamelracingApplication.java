5080d16feeeeec2953d12e894554614d67327a1a
Juan Jose Jaramillo <juan.jaramillo14@eia.edu.co>
2026-09-09 12:39:42 -0500

    chore: estructura inicial del proyecto Spring Boot

---
+++ src/main/java/com/eia/camelracing/CamelracingApplication.java
@@ -0,0 +1,13 @@
+package com.eia.camelracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelracingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eia/camelracing/CamelracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eia.camelracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
